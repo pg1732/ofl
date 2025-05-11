da35afd021636d6871e5cb0efe195e05a3cdc30b
patrickgonzales <patrickgonzales@mac.attlocal.net>
2025-05-10 17:22:24 -0700

    restructured github correctly

---
+++ src/ofl/ofl.java
@@ -0,0 +1,55 @@
+package ofl;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ofl extends JFrame {
+
+	
+	public ofl() {
+		super("OceanFloorLandforms");
+        this.setLayout(new FlowLayout());
+        
+		JPanel buttonPanel = new JPanel();
+		GridLayout buttonLayout = new GridLayout(4,1,5,5);
+		buttonPanel.setLayout(buttonLayout);
+		JButton plainsButton = new JButton("plains");
+		JButton shelfButton = new JButton("shelf");
+		JButton ridgeButton = new JButton("ridge");
+		JButton trenchButton = new JButton("trench");
+		
+		buttonPanel.add(plainsButton);
+		//plainsButton.addActionListener();
+		buttonPanel.add(shelfButton);
+		//shelfButton.addActionListener();
+		buttonPanel.add(ridgeButton);
+		//ridgeButton.addActionListener();
+		buttonPanel.add(trenchButton);
+		//trenchButton.addActionListener();
+		this.add(buttonPanel);
+		
+		JPanel dataPanel = new JPanel();
+		dataPanel.setLayout(new GridLayout(2,1,5,5));
+		JLabel waterLabel = new JLabel("Water Depth");
+		JTextField waterField = new JTextField(40);
+		
+		dataPanel.add(waterLabel);
+		dataPanel.add(waterField);
+
+		add(dataPanel);
+		this.setSize(400, 500);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ofl();
+	}
+
+}
