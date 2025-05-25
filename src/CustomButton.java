faf24cdbd18a5406c8279b8a7bccff0b3f5e13ce
patrickgonzales <patrickgonzales@mac.attlocal.net>
2025-05-24 18:14:52 -0700

    Made progress on the code and fixed a GUI problem because it didnt run

--- src/CustomButton.java
+++ src/CustomButton.java
@@ -6,33 +6,28 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
-public class customButton extends JButton implements ActionListener{
+public class CustomButton extends JButton implements ActionListener{
 	private JTextField waterField;
 	private JTextField featuresField;
 	private JTextField productivityField;
 	private JTextField habitatField;
-	private String waterText;
-	private String featuresText;
-	private String productivityText;
-	private String habitatText;
 	private String buttonName;
+	private String[] texts;
 	
-	public customButton(String buttonName)
+	public CustomButton(String buttonName)
 	{
 		super(buttonName);
 		addActionListener(this);
 		this.buttonName = buttonName;
-		waterField = new JTextField();
-		waterField.setEditable(false);
+		this.texts = new String[4];
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		waterField.setText(this.waterText);
-		featuresField.setText(this.featuresText);
-		productivityField.setText(this.productivityText);
-		habitatField.setText(this.habitatText);
-		
+		waterField.setText(this.texts[0]);
+		featuresField.setText(this.texts[1]);
+		productivityField.setText(this.texts[2]);
+		habitatField.setText(this.texts[3]);
 	}
 
 	public String getButtonName() {
@@ -43,40 +38,20 @@ public class customButton extends JButton implements ActionListener{
 		this.buttonName = buttonName;
 	}
 
-	public String getWaterText() {
-		return waterText;
+	public String getText(int idx            ) { return texts[idx]; }
+	public void   setText(int idx,String text) { texts[idx]=text;   }
+	
+	public String toString()
+	{
+		String fields ="";
+		for(int idx=0; idx<4; idx++)
+			fields += texts[idx]+"'";
+		return this.buttonName+": "+fields;
 	}
-
-	public void setWaterText(String waterText) {
-		this.waterText = waterText;
-	}
-
-	public String getFeaturesText() {
-		return featuresText;
-	}
-
-	public void setFeaturesText(String featuresText) {
-		this.featuresText = featuresText;
-	}
-
-	public String getProductivityText() {
-		return productivityText;
-	}
-
-	public void setProductivityText(String productivityText) {
-		this.productivityText = productivityText;
-	}
-
-	public String getHabitatText() {
-		return habitatText;
-	}
-
-	public void setHabitatText(String habitatText) {
-		this.habitatText = habitatText;
-	}
+	
 
 	public JTextField getWaterField() {
-		return waterField;
+		return this.waterField;
 	}
 
 	public void setWaterField(JTextField waterField) {
@@ -107,5 +82,5 @@ public class customButton extends JButton implements ActionListener{
 		this.habitatField = habitatField;
 	}
 	
-	
+
 }
--- src/ofl.java
+++ src/ofl.java
@@ -3,6 +3,10 @@ package src;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
@@ -12,64 +16,75 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class ofl extends JFrame {
+	private JTextField waterField;
+	private JTextField featuresField;
+	private JTextField productivityField;
+	private JTextField habitatField;
 
 	public ofl() {
 		super("OceanFloorLandforms");
         this.setLayout(new GridLayout(4,1,5,5));
-        
 		JPanel buttonPanel = new JPanel();
 		GridLayout buttonLayout = new GridLayout(4,1,5,5);
 		buttonPanel.setLayout(buttonLayout);
-		customButton plainsButton = new customButton("plains");
-		customButton shelfButton = new customButton("shelf");
-		customButton ridgeButton = new customButton("ridge");
-		customButton trenchButton = new customButton("trench");
+        
+        ArrayList<CustomButton> buttons = new ArrayList<CustomButton>();
+        waterField = new JTextField();
+        featuresField = new JTextField();
+        productivityField = new JTextField();
+		habitatField = new JTextField();
+
+    	
 		
-		plainsButton.setWaterText("10,000 feet");
-		plainsButton.setFeaturesText("Hills, valleys, and seamounts");
-		plainsButton.setProductivityText("The largest habitat on earth and less productive then the continental shelf");
-		plainsButton.setHabitatText("Biodiversity and a wide variety of life");
-		buttonPanel.add(plainsButton);
-		shelfButton.setWaterText("A few hundred feet deep");
-		shelfButton.setFeaturesText("Guyots, submarine volcanic fields, interacting fans, seafloor channels, levees, and landslide scarps");
-		shelfButton.setProductivityText("Relatively productive in light and nutrients");
-		shelfButton.setHabitatText("None");
-		buttonPanel.add(shelfButton);
-		ridgeButton.setWaterText("8,000 feet");
-		ridgeButton.setFeaturesText("Hills, valleys, and seamounts(uderwater mountains");
-		ridgeButton.setProductivityText("The largest habitat on earth. New floor is created as tectonic plates spread apart. There are many volcanoes that cover the mid ocean ridge(longest mountain range on earth).");
-		ridgeButton.setHabitatText("None");
-		buttonPanel.add(ridgeButton);
-		trenchButton.setWaterText("36,201 feet");
-		trenchButton.setFeaturesText("Deepest place in the ocean");
-		trenchButton.setProductivityText("None");
-		trenchButton.setHabitatText("None");
-		buttonPanel.add(trenchButton);
-		
+		File myFile = new File("datafile.txt"); // fileName is the name of the file
+		Scanner scan = null;
+		try {
+			scan = new Scanner(myFile); // Create a Scanner object using the File object
+			for(int b=0; b<4; b++)
+			{
+				String name = scan.nextLine();
+				buttons.add(new CustomButton(name));
+				for(int f=0; f<4;f++)
+				{
+					String field = scan.nextLine();
+					buttons[b].setText(f,field);
+				}
+				System.out.println("setup button:"+buttons[b]);
+				buttons[b].setWaterField(waterField);
+				buttons[b].setFeaturesField(featuresField);
+				buttons[b].setProductivityField(productivityField);
+				buttons[b].setHabitatField(habitatField);
+				buttonPanel.add(buttons[b]);
+
+			}
+		} 
+		catch (FileNotFoundException e) { e.printStackTrace(); } 
+		finally { if (scan != null) scan.close(); }
 		this.add(buttonPanel);
 		
+	  		
 		JPanel dataPanel = new JPanel();
 		dataPanel.setLayout(new GridLayout(4,2,5,5));
 		
 		JLabel waterLabel = new JLabel("Water Depth");
 		dataPanel.add(waterLabel);
-		dataPanel.add(plainsButton.getWaterField());
+	    dataPanel.add(waterField);
 		
-//		JLabel featuresLabel = new JLabel("Features");
-//		dataPanel.add(featuresLabel);
-//		dataPanel.add(plainsButton.getFeaturesField());
-//		
-//		JLabel productivityLabel = new JLabel("Productivity");
-//		dataPanel.add(productivityLabel);
-//		dataPanel.add(plainsButton.getProductivityField());
-//		
-//		JLabel habitatLabel = new JLabel("Habitat");
-//		dataPanel.add(habitatLabel);
-//		dataPanel.add(plainsButton.getHabitatField());
-//		
-//		JPanel textFieldPanel = new JPanel();
-//		textFieldPanel.setLayout(new GridLayout(0,1,5,5));
-//		textFieldPanel.add(new JTextField("Water Depth"));
+		JLabel featuresLabel = new JLabel("Features");
+		dataPanel.add(featuresLabel);
+	    dataPanel.add(featuresField);
+		
+		JLabel productivityLabel = new JLabel("Productivity");
+		dataPanel.add(productivityLabel);
+		dataPanel.add(productivityField);
+		
+		JLabel habitatLabel = new JLabel("Habitat");
+		dataPanel.add(habitatLabel);
+		dataPanel.add(habitatField);
+		
+		JPanel textFieldPanel = new JPanel();
+		textFieldPanel.setLayout(new GridLayout(0,1,5,5));
+		textFieldPanel.add(new JTextField("Water Depth"));
 
 		add(dataPanel);
 		this.setSize(400, 500);
